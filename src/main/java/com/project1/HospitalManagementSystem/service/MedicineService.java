b61d49805f91d61c25bc82fd81869749cbc19839
Mohd Kaif <kaif85994@gmail.com>
2026-02-12 22:32:18 +0530

    Delete src directory

--- src/main/java/com/project1/HospitalManagementSystem/service/MedicineService.java
+++
@@ -1,9 +0,0 @@
-package com.project1.HospitalManagementSystem.service;
-
-import com.project1.HospitalManagementSystem.model.Medicine;
-
-public interface MedicineService {
-    void addMedicine(Medicine medicine);
-    void viewAllMedicine();
-    void delete(int id);
-}
